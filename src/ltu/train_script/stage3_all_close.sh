2c262a161bd0535d79bd4e213fc079ea24af5c71
Akhil <bakhil1997@gmail.com>
2025-05-09 03:36:35 -0500

    Scripts for running stage 2 and stage 3

--- src/ltu/train_script/stage3_all_close.sh
+++ src/ltu/train_script/stage3_all_close.sh
@@ -9,18 +9,28 @@
 #SBATCH --mem=470000
 #SBATCH --exclusive
 
+JUMP_TO_LAYER=8
+AUDIO_FILES_PATH_PREFIX="../../.."
+DATA_JSON_PATH=../../../openaqa/data/closed_ended/all_closed_qa_filtered.json
+
 export TRANSFORMERS_CACHE=./hf_cache/
 export HF_DATASETS_CACHE=./hf_cache/
-output_dir='../exp/stage3_all_close'
+
+output_dir="../exp/JUMP_TO_${JUMP_TO_LAYER}/stage3_all_close"
 mkdir -p $output_dir
 cp "$0" ${output_dir}/$(date +"%Y-%m-%d-%H-%M-%S").sh
 
-torchrun --nproc_per_node=4 --master_port=1234 ../finetune.py \
-    --base_model '/data/sls/scratch/yuangong/ltu/src/ltu/exp/stage2_all_cla/checkpoint-9200/pytorch_model.bin' \
-    --data_path '../../../openaqa/data/closed_ended/all_closed_qa.json' \
+
+NPROC_PER_NODE=$(python -c 'import torch; print(torch.cuda.device_count())')
+
+torchrun --nproc_per_node=$NPROC_PER_NODE --master_port=1234 ../finetune_jump.py \
+    --base_model "../exp/JUMP_TO_${JUMP_TO_LAYER}/stage2_all_cla/checkpoint-8400/pytorch_model.bin" \
+    --data_path $DATA_JSON_PATH \
+    --audio_files_path_prefix $AUDIO_FILES_PATH_PREFIX \
+    --jump_to_layer $JUMP_TO_LAYER \
     --output_dir $output_dir \
     --batch_size 256 \
-    --micro_batch_size 4 \
+    --micro_batch_size 16 \
     --num_epochs 1 \
     --learning_rate 1e-4 \
     --cutoff_len 108 \
@@ -31,6 +41,5 @@ torchrun --nproc_per_node=4 --master_port=1234 ../finetune.py \
     --lora_target_modules '[q_proj,v_proj]' \
     --train_on_inputs \
     --group_by_length \
-    --wandb_run_name ${output_dir} \
-    --save_steps 2000 \
+    --save_steps 2200 \
     --trainable_params all
